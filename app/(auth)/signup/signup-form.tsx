628895b0bfc6f2d02da4e3d59b3556efb67b91bb
spagestic <steelmax666@gmail.com>
2026-02-28 12:00:14 +0800

    add Google login button to login and signup forms

--- app/(auth)/signup/signup-form.tsx
+++ app/(auth)/signup/signup-form.tsx
@@ -74,7 +74,7 @@ export function SignupForm({
     setLoading(true);
     try {
       await signIn("password", formData);
-      router.push("/");
+      router.push("/game");
     } catch (err) {
       const errorMessage = getErrorMessage(err);
       setError(errorMessage);
@@ -117,18 +117,14 @@ export function SignupForm({
             placeholder="m@example.com"
             required
           />
-          <FieldDescription>
-            We&apos;ll use this to contact you. We will not share your email
-            with anyone else.
-          </FieldDescription>
         </Field>
 
         <Field>
           <FieldLabel htmlFor="password">Password</FieldLabel>
           <Input id="password" name="password" type="password" required />
-          <FieldDescription>
+          {/* <FieldDescription>
             Must be at least 8 characters long.
-          </FieldDescription>
+          </FieldDescription> */}
         </Field>
 
         <Field>
@@ -139,7 +135,7 @@ export function SignupForm({
             type="password"
             required
           />
-          <FieldDescription>Please confirm your password.</FieldDescription>
+          {/* <FieldDescription>Please confirm your password.</FieldDescription> */}
         </Field>
 
         {/* Hidden flow field that Convex uses to distinguish sign-up */}
@@ -153,6 +149,19 @@ export function SignupForm({
 
         <FieldSeparator>Or continue with</FieldSeparator>
         <Field>
+          <Button
+            variant="outline"
+            type="button"
+            onClick={() => void signIn("google")}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
+              <path
+                d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                fill="currentColor"
+              />
+            </svg>
+            Login with Google
+          </Button>
           <Button
             variant="outline"
             type="button"
